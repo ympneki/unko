4f7cf3df1320c18bf0124039ca3ebd0d7d22245d
813 <github@813.jp>
2017-07-14 17:28:16 +0900

    ぶりぶりするようにした

--- unko.cpp
+++ unko.cpp
@@ -2,13 +2,18 @@
 #include <cstdlib>
 
 int main() {
-	int unkoN = 100;
+	double unkoAmount = 0.7;	// 0.0から1.0の実数
 	
 	std::cout << "Hello, Unko!!" << std::endl;
 	std::cout << "Hello, Geri" << std::endl;
 
-	cout << "My unko Num : " << unkoN << endl;
+	cout << "My unko Num : " << unkoAmount << endl;
 
+	if( unkoAmount >= 1.0 ) {
+		std::cout << "Buri Buri" std::endl;
+		unkoAmount -= 1.0;
+	}
+		
 	return EXIT_SUCCESS;
 
 }
